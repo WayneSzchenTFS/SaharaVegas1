3506c5204ad727bd90301b2ba013a2a2e987fdd4
waynechen <szchen@tfs.ca>
2023-03-31 09:12:46 -0400

    Initial Commit

---
+++ SaharaVegas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaharaVegas
+//
+//  Created by Wayne Chen on 2023-03-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
