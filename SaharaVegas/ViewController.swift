d2a071a453a112ce9cca9ef763f10596567ada6c
waynechen <szchen@tfs.ca>
2023-04-01 16:17:45 -0400

    Il y a une problème avec les constraints des logos, images. Il dit qu'il existe des contraintes conflictuelles mais je ne peux pas les trouver

--- SaharaVegas/ViewController.swift
+++ SaharaVegas/ViewController.swift
@@ -8,13 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-//
-    @IBOutlet weak var diceImageView: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DOne")
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DOne")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton)
+    {
+        let DiceArray = [UIImage(imageLiteralResourceName: "DOne.png"), UIImage(imageLiteralResourceName: "DTwo.png"), UIImage(imageLiteralResourceName: "DThree.png"), UIImage(imageLiteralResourceName: "DFour.png"), UIImage(imageLiteralResourceName: "DFive.png"), UIImage(imageLiteralResourceName: "DSix.png")]
+        diceImageView1.image = DiceArray.randomElement()
+        diceImageView2.image = DiceArray[Int.random(in: 0...5)]
+    }
+    
 }
 
